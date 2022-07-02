a40b8f7518a4b5e35c7f0409d2965970e523c98f
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-07-02 18:15:07 -0500

    chore: moving audio

--- src/respond.py
+++ src/respond.py
@@ -3,5 +3,5 @@ from gtts import gTTS
 
 def respond(audioString):
     tts = gTTS(text=audioString, lang="en")
-    tts.save("speech.mp3")
+    tts.save("./util/audio/speech.mp3")
     os.system("mpg321 speech.mp3 >/dev/null 2>&1")
