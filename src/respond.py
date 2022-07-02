6150fafc0a85b9160e52e6a0df9f2efe0e541273
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-07-02 18:06:11 -0500

    chore: adding output suppressor

--- src/respond.py
+++ src/respond.py
@@ -4,4 +4,4 @@ from gtts import gTTS
 def respond(audioString):
     tts = gTTS(text=audioString, lang="en")
     tts.save("speech.mp3")
-    os.system("mpg321 speech.mp3")
+    os.system("mpg321 speech.mp3 >/dev/null 2>&1")
---
+++ src/suppress_stdout.py
@@ -0,0 +1,43 @@
+import sys, os
+
+class suppress_output:
+    def __init__(self, suppress_stdout=True, suppress_stderr=True):
+        self.suppress_stdout = suppress_stdout
+        self.suppress_stderr = suppress_stderr
+        self._stdout = None
+        self._stderr = None
+
+    def __enter__(self):
+        devnull = open(os.devnull, "w")
+        if self.suppress_stdout:
+            self._stdout = sys.stdout
+            sys.stdout = devnull
+
+        if self.suppress_stderr:
+            self._stderr = sys.stderr
+            sys.stderr = devnull
+
+    def __exit__(self, *args):
+        if self.suppress_stdout:
+            sys.stdout = self._stdout
+        if self.suppress_stderr:
+            sys.stderr = self._stderr
+
+from contextlib import contextmanager
+
+@contextmanager
+def nullify_output(suppress_stdout=True, suppress_stderr=True):
+    stdout = sys.stdout
+    stderr = sys.stderr
+    devnull = open(os.devnull, "w")
+    try:
+        if suppress_stdout:
+            sys.stdout = devnull
+        if suppress_stderr:
+            sys.stderr = devnull
+        yield
+    finally:
+        if suppress_stdout:
+            sys.stdout = stdout
+        if suppress_stderr:
+            sys.stderr = stderr
