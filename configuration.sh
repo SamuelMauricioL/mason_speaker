706fb738ec4d297470f254d327ccf88e1735b1f1
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-07-02 12:35:03 -0500

    chore: adding SpeechRecognition package

--- configuration.sh
+++ configuration.sh
@@ -6,9 +6,11 @@ echo "✔️ (mason-speaker) environment created 🔥"
 . venv/bin/activate
 
 echo ""
-echo "Installing packages 📦 in (mason-speaker)"
+echo "Installing packages 📦 in env (mason-speaker)"
 # sudo pip install -r requirements.txt >/dev/null
 pip install gTTS==2.2.4 >/dev/null
 echo "✔️ gTTS"
+pip install SpeechRecognition==3.8.1 >/dev/null
+echo "✔️ SpeechRecognition"
 
 echo ""
