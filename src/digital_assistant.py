78d3dc85b9c7490eb6d07872c36e3fdd6c342690
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-07-02 19:51:19 -0500

    chore: preparing create command

--- src/digital_assistant.py
+++ src/digital_assistant.py
@@ -9,6 +9,14 @@ def digital_assistant(data):
     if "what time is it" in data:
         listening = True
         respond(ctime())
+    
+    if "create" in data:
+        data = data.split(" ")
+        command = str(data[1])
+        if "template" in command:
+            listening = True
+            brick = str(data[2:])
+            respond(brick)
 
     if "stop listening" in data:
         respond("See you")
