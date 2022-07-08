1613cdea671118d32d016715a518b1f93772e98f
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-07-08 15:00:08 -0500

    chore: new commands

--- src/digital_assistant.py
+++ src/digital_assistant.py
@@ -1,3 +1,4 @@
+import os
 from time import ctime
 from src.respond import respond
 from src.recognize_speech_from_mic import RecognizeSpeech
@@ -10,17 +11,39 @@ def digital_assistant(data):
     if "what time is it" in data:
         respond(ctime())
     
-    if "create a template" in data:
-        # data = data.split(" ")
-        # command = str(data[1])
-        respond("ok, what is the name of the template?")
+    if "search a template" in data:
+        respond("looking on brickhub, what is the name of the brick?")
         recognizeSpeech = RecognizeSpeech();
         response = recognizeSpeech.from_mic()
+        # brick = input('What is the name of the brick? 🧱\n')
         if (response['ok'] != None):
-            respond("looking for " + response['ok'] + " on brickhub")
+            os.system("mason search " + response['ok'])
+        # os.system("mason add -g " + brick)
+        # os.system("mason make " + brick)
+        
+        # if (response['ok'] != None):
 
-        if (response['error'] != None):
-            respond("template not found")
+    if "execute" in data:
+        data = data.replace('execute', '')
+        command = data.lower()
+        print("mason search " + command)
+        os.system(command)
+
+    if "repository state" in data:
+        output = os.popen("git status").read()
+        if "no changes added to commit" in output:
+            respond("you have changes to upload")
+        if "nothing to commit" in output:
+            respond("no changes to upload")
+        print(output)
+    
+    if "upload my changes" in data:
+        respond("Which is the message?")
+        message = input('git commit -m ')
+        os.system("git add .")
+        os.system("git commit -m " + '"' + message + '"')
+        os.system("git push")
+        respond("cool, uploaded changes")
 
     if "see you" in data:
         respond("See you")
