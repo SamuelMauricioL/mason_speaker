dbaf16c0f6e39f6523824c80523f9e7e99cd095e
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-07-08 09:58:43 -0500

    chore: add Singleton to class RecognizeSpeech

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
-import speech_recognition as sr
-
 from src.respond import respond
 from src.digital_assistant import digital_assistant
-from src.recognize_speech_from_mic import recognize_speech_from_mic
+from src.recognize_speech_from_mic import RecognizeSpeech
 
 from src.prints.print_hi import print_hi
 from src.prints.print_error import print_error
-from src.prints.print_verified_microphone import print_verified_microphone
 
 if __name__ == "__main__":
     print_hi()
-    recognizer = sr.Recognizer()
-    mic = sr.Microphone()
-    print_verified_microphone()
+    recognizeSpeech = RecognizeSpeech();
 
     isListening = True
     while isListening == True:
-        response = recognize_speech_from_mic(recognizer, mic)
+        response = recognizeSpeech.from_mic()
         if (response['ok'] != None):
             isListening = digital_assistant(response['ok'])
 
--- src/recognize_speech_from_mic.py
+++ src/recognize_speech_from_mic.py
@@ -1,31 +1,43 @@
 import speech_recognition as sr
+from src.prints.print_verified_microphone import print_verified_microphone
 
-def recognize_speech_from_mic(recognizer, microphone):
-    
-    if not isinstance(recognizer, sr.Recognizer):
-        raise TypeError("`recognizer` must be `Recognizer` instance")
+class RecognizeSpeech:
+    def __init__(self):
+        self.recognizer = sr.Recognizer()
+        self.microphone = sr.Microphone()
+        print_verified_microphone()
 
-    if not isinstance(microphone, sr.Microphone):
-        raise TypeError("`microphone` must be `Microphone` instance")
+    def __new__(self):
+        if not hasattr(self, 'instance'):
+            self.instance = super().__new__(self)
+        return self.instance
+
+    def from_mic(self):
         
-    with microphone as source:
-        print("I'm listening 👂")
-        recognizer.adjust_for_ambient_noise(source) # #  analyze the audio source for 1 second
-        audio = recognizer.listen(source)
+        if not isinstance(self.recognizer, sr.Recognizer):
+            raise TypeError("`recognizer` must be `Recognizer` instance")
+
+        if not isinstance(self.microphone, sr.Microphone):
+            raise TypeError("`microphone` must be `Microphone` instance")
+            
+        with self.microphone as source:
+            print("I'm listening 👂")
+            self.recognizer.adjust_for_ambient_noise(source) # #  analyze the audio source for 1 second
+            audio = self.recognizer.listen(source)
+            
+        response = {
+            "ok": None,
+            "error": None,
+        }
+        try:
+            response["ok"] = self.recognizer.recognize_google(audio)
+            print("You said: " + response["ok"])
+        except sr.RequestError as e:
+            # response["error"] = "Request Failed; {0}".format(e)
+            response["ok"] = None
+            response["error"] = "I didn't understand"
+        except sr.UnknownValueError:
+            response["ok"] = None
+            response["error"] = "Can you say it again?"
         
-    response = {
-        "ok": None,
-        "error": None,
-    }
-    try:
-        response["ok"] = recognizer.recognize_google(audio)
-        print("You said: " + response["ok"])
-    except sr.RequestError as e:
-        # response["error"] = "Request Failed; {0}".format(e)
-        response["ok"] = None
-        response["error"] = "I didn't understand"
-    except sr.UnknownValueError:
-        response["ok"] = None
-        response["error"] = "Can you say it again?"
-    
-    return response
+        return response
