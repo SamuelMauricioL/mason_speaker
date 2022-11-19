a5a71dde16594ee667d611a08710d2bed21bd358
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-11-19 18:50:21 -0500

    chore: suport respond to languages(Spanish, English, Portuguese)

--- src/utils/respond.py
+++ src/utils/respond.py
@@ -1,13 +1,28 @@
 import os
 from gtts import gTTS
 from src.utils.logger import logger, TypeOfRemitter
+from src.core.global_configuration import GlobalConfiguration
+from src.core.languages import TypeOfLanguage
 
 
 def respond(audioString):
     # register log
     logger(TypeOfRemitter.AI, audioString)
+
     # convert message to audio
-    tts = gTTS(text=audioString, lang="en")
+    type_of_language = GlobalConfiguration().get_language()
+    language = get_gTTS_language(type_of_language)
+    tts = gTTS(text=audioString, lang=language)
     tts.save("./util/audio/speech.wav")
+
     # replace comando with mpg321 if is linux
     os.system("afplay ./util/audio/speech.wav >/dev/null 2>&1")
+
+
+def get_gTTS_language(type_of_language):
+    if type_of_language == TypeOfLanguage.ENGLISH:
+        return 'en'
+    elif type_of_language == TypeOfLanguage.SPANISH:
+        return 'es'
+    elif type_of_language == TypeOfLanguage.PORTUGUESE:
+        return 'pt'
