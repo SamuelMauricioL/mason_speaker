928783c0f38b668beb7713ed2ab72b09c7d1cbe4
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-11-19 19:45:22 -0500

    chore: add new dependecies to linux_install.sh

---
+++ linux_install.sh
@@ -0,0 +1,23 @@
+echo "Creating environment 🏕️"
+python -m venv env --prompt="mason-speaker" &>/dev/null
+echo "✔️ (mason-speaker) environment created 🔥"
+. env/bin/activate
+
+echo ""
+echo "Installing python packages 📦 in env (mason-speaker)"
+pip install pyaudio==0.2.12 &>/dev/null
+echo "✔️ pyaudio"
+pip install gTTS==2.2.4 &>/dev/null
+echo "✔️ gTTS"
+pip install wavio==0.0.7 &>/dev/null
+echo "✔️ wavio"
+pip install sounddevice==0.4.5 &>/dev/null
+echo "✔️ sounddevice"
+pip install SpeechRecognition==3.8.1 &>/dev/null
+echo "✔️ SpeechRecognition"
+pip install git+https://github.com/openai/whisper.git &>/dev/null
+echo "✔️ Whisper"
+
+mkdir -p util/audio
+echo ""
+echo "Installed a Mason Speaker! 🧱🎤"
