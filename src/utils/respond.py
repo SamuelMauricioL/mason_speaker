9691bbfb0049af419ad20c864ce65c554e501269
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-11-19 19:07:05 -0500

    refactor: spaces

--- src/utils/respond.py
+++ src/utils/respond.py
@@ -12,10 +12,10 @@ def respond(audioString):
     # convert message to audio
     type_of_language = GlobalConfiguration().get_language()
     language = get_gTTS_language(type_of_language)
+
     try:
         tts = gTTS(text=audioString, lang=language)
         tts.save("./util/audio/speech.wav")
-
         # replace comando with mpg321 if is linux
         os.system("afplay ./util/audio/speech.wav >/dev/null 2>&1")
     except:
