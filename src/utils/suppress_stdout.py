63114ad868c4f85262b28d966e2ba18c70da725b
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-11-25 18:35:51 -0500

    chore: improve suppress_stdout_stderr

--- src/utils/suppress_stdout.py
+++ src/utils/suppress_stdout.py
@@ -1,47 +1,10 @@
-import sys, os
-
-
-class suppress_output:
-
-    def __init__(self, suppress_stdout=True, suppress_stderr=True):
-        self.suppress_stdout = suppress_stdout
-        self.suppress_stderr = suppress_stderr
-        self._stdout = None
-        self._stderr = None
-
-    def __enter__(self):
-        devnull = open(os.devnull, "w")
-        if self.suppress_stdout:
-            self._stdout = sys.stdout
-            sys.stdout = devnull
-
-        if self.suppress_stderr:
-            self._stderr = sys.stderr
-            sys.stderr = devnull
-
-    def __exit__(self, *args):
-        if self.suppress_stdout:
-            sys.stdout = self._stdout
-        if self.suppress_stderr:
-            sys.stderr = self._stderr
-
-
-from contextlib import contextmanager
+from contextlib import contextmanager, redirect_stderr, redirect_stdout
+from os import devnull
 
 
 @contextmanager
-def nullify_output(suppress_stdout=True, suppress_stderr=True):
-    stdout = sys.stdout
-    stderr = sys.stderr
-    devnull = open(os.devnull, "w")
-    try:
-        if suppress_stdout:
-            sys.stdout = devnull
-        if suppress_stderr:
-            sys.stderr = devnull
-        yield
-    finally:
-        if suppress_stdout:
-            sys.stdout = stdout
-        if suppress_stderr:
-            sys.stderr = stderr
+def suppress_stdout_stderr():
+    """A context manager that redirects stdout and stderr to devnull"""
+    with open(devnull, 'w') as fnull:
+        with redirect_stderr(fnull) as err, redirect_stdout(fnull) as out:
+            yield (err, out)
